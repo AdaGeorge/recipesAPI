6495c1fd001eddbeabe26f64d11296e5e7bbf97d
Ada George <adageorge79@gmail.com>
2022-11-03 21:52:57 -0500

    database dialectOptions

--- src/utils/database.js
+++ src/utils/database.js
@@ -7,7 +7,15 @@ const db = new Sequelize({
     host: config.db.host, //? Variable de entorno del host
     username: config.db.userName, //? Variable de entorno del usuario
     password: config.db.password, //? Variable de entorno de la contraseña
-    database: config.db.name //? Variable de entorno de la base de datos
+    database: config.db.name, //? Variable de entorno de la base de datos
+    dialectOptions: 
+        process.env.NODE_ENV === 'production'
+            ? {
+                ssl: {
+                    require: true,
+                    rejectUnauthorized: false
+                }
+            } : {},
 })
 
 module.exports = db
